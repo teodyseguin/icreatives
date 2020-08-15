2a6df93b944a04d22861c5d2345e3271494fa134
Teody C. Seguin <teodyseguin@gmail.com>
2020-08-15 14:51:20 +0800

    Update on component files

---
+++ web/themes/custom/particle/apps/pl-default/pattern-lab/_patterns/01-atoms-demo/rounded-box/index.js
@@ -0,0 +1,13 @@
+/**
+ * Demo of rounded-box. Pulls in rounded-box assets, and provides demo-only assets.
+ *
+ * (This file is NOT imported by the design system, but is included as part of
+ * a Pattern Lab app.)
+ */
+
+// Import component assets
+import 'atoms/rounded-box';
+
+// Import demo assets
+import './rounded-boxs.twig';
+import './rounded-boxs.yml';
---
+++ web/themes/custom/particle/source/default/_patterns/01-atoms/rounded-box/__tests__/rounded-box.test.js
@@ -0,0 +1,5 @@
+import { name } from '..';
+
+test('rounded-box component is registered', () => {
+  expect(name).toBe('rounded-box');
+});
