939941b7df70b2414b40170c3e7d2f8fd1d354e8
Teody C. Seguin <teodyseguin@gmail.com>
2020-08-21 14:53:26 +0800

    Update particle theme

--- web/themes/custom/particle/apps/drupal-default/particle_theme/js/client-filter.js
+++ web/themes/custom/particle/apps/drupal-default/particle_theme/js/client-filter.js
@@ -1,10 +1,12 @@
 (function clientFilterModule($) {
-  const selectedClient = localStorage.getItem('selectedClient');
+  if (drupalSettings.path.hasOwnProperty('currentQuery')) {
+    const selectedClient = localStorage.getItem('selectedClient');
 
-  // If selectedClient is available from local storage,
-  // then we set that value as a default for the client filter.
-  if (selectedClient) {
-    $('.filter .selectbox select').val(selectedClient);
+    // If selectedClient is available from local storage,
+    // then we set that value as a default for the client filter.
+    if (selectedClient) {
+      $('.filter .selectbox select').val(selectedClient);
+    }
   }
 
   // Event for the client filter button.
