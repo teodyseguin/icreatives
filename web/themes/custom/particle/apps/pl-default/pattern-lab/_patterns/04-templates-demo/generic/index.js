f44bdcc3fac0f8066143dc2f3adba0b49eb7b20c
Teody C. Seguin <teodyseguin@gmail.com>
2020-08-25 15:33:06 +0800

    Create generic page template

---
+++ web/themes/custom/particle/apps/pl-default/pattern-lab/_patterns/04-templates-demo/generic/index.js
@@ -0,0 +1,13 @@
+/**
+ * Demo of generic. Pulls in generic assets, and provides demo-only assets.
+ *
+ * (This file is NOT imported by the design system, but is included as part of
+ * a Pattern Lab app.)
+ */
+
+// Import component assets
+import 'templates/generic';
+
+// Import demo assets
+import './generics.twig';
+import './generics.yml';
---
+++ web/themes/custom/particle/source/default/_patterns/04-templates/generic/__tests__/generic.test.js
@@ -0,0 +1,5 @@
+import { name } from '..';
+
+test('generic component is registered', () => {
+  expect(name).toBe('generic');
+});
