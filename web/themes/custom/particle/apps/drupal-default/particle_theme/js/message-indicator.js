14f9649f9ad5f3192a6922b2370bd1a1eb6ffdad
Teody C. Seguin <teodyseguin@gmail.com>
2020-09-08 19:07:21 +0800

    Add logic to indicate new message

---
+++ web/themes/custom/particle/apps/drupal-default/particle_theme/js/message-indicator.js
@@ -0,0 +1,66 @@
+(function messageIndicator($) {
+  const getMessageCount = () => {
+    const { messages } = drupalSettings.ic;
+    let messageCount = 0;
+
+    if (Object.keys(messages).length) {
+      Object.keys(messages).forEach(function iterate(value) {
+        if (messages[value]) {
+          messageCount += messages[value];
+        }
+      });
+    }
+
+    return messageCount;
+  };
+
+  const indicateOnSidebar = () => {
+    const $blockNavigation = $('#block-mainnavigation');
+    const messageCount = getMessageCount();
+
+    // If block main navigation is not present on the page,
+    // then we don't need to do anything below.
+    if (!$blockNavigation.length) {
+      return;
+    }
+
+    // Same goes when the message count is zero.
+    if (!messageCount) {
+      return;
+    }
+
+    $blockNavigation.find('li a').each(function findMenuItem() {
+      const $self = $(this);
+      const path = $self.attr('data-drupal-link-system-path');
+
+      if (path === 'inbox') {
+        const $parent = $self.parent();
+
+        $parent.append(
+          `<span class="message-count font-sansserif">${messageCount}</span>`
+        );
+      }
+    });
+  };
+
+  const indicateOnPage = () => {
+    const { currentPath } = drupalSettings.path;
+    const { messages } = drupalSettings.ic;
+
+    if (!currentPath || currentPath !== 'inbox') {
+      return;
+    }
+
+    if (Object.keys(messages).length) {
+      Object.keys(messages).forEach(function iterate(value) {
+        if ($(`[data-id="${value}"]`).length && messages[value]) {
+          $(`[data-id="${value}"]`).addClass('font-bold');
+        }
+      });
+    }
+  };
+
+  indicateOnSidebar();
+  indicateOnPage();
+
+})(jQuery);
