a2ae39e449d1e97747ab6876cb68eeeecd4c0c43
Teody C. Seguin <teodyseguin@gmail.com>
2020-09-26 08:36:18 +0800

    Create date range component

---
+++ web/themes/custom/particle/apps/pl-default/pattern-lab/_patterns/02-molecules-demo/date-range/index.js
@@ -0,0 +1,13 @@
+/**
+ * Demo of date-range. Pulls in date-range assets, and provides demo-only assets.
+ *
+ * (This file is NOT imported by the design system, but is included as part of
+ * a Pattern Lab app.)
+ */
+
+// Import component assets
+import 'molecules/date-range';
+
+// Import demo assets
+import './date-ranges.twig';
+import './date-ranges.yml';
---
+++ web/themes/custom/particle/source/default/_patterns/02-molecules/date-range/__tests__/date-range.test.js
@@ -0,0 +1,5 @@
+import { name } from '..';
+
+test('date-range component is registered', () => {
+  expect(name).toBe('date-range');
+});
